6cc97f4b2a6ffedd7e92176682a97f0979a7df77
Kaoru Fukumoto <k1fukumoto@gmail.com>
2019-10-02 09:42:10 +0000

    run 2nd phase script at the end

--- onboard/onboard_user.sh
+++ onboard/onboard_user.sh
@@ -9,4 +9,4 @@ sudo su $USER  -c 'ssh-keygen -t rsa -N "" -f /home/$USER/.ssh/id_rsa'
 
 cd /home/$USER
 sudo su $USER -c 'git clone https://github.com/k1fukumoto/tanzu-lab.git'
-
+sudo su $USER -c "./tanzu-lab/onboard/onboard_user_2.sh $USER"
