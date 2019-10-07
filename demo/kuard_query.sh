812ae7f7033134a73107a00801ef53f20740d178
Kaoru Fukumoto <k1fukumoto@gmail.com>
2019-10-07 11:32:14 +0000

    print the list of pods

--- demo/kuard_query.sh
+++ demo/kuard_query.sh
@@ -4,6 +4,9 @@ while true
 do
   POD=$(curl -s http://10.108.5.201:8080 | grep pageCon | sed s/^.*\{/{/ |  python3 -c "import sys, json; print(json.load(sys.stdin)['hostname'])")
   IMAGE=$(kubectl get pod $POD -o jsonpath="{.spec.containers[0]['image']}")
-  echo $POD $IMAGE
+
+  echo
+  echo "->" $POD '('$IMAGE')'
+  kubectl get pod --selector run=kuard
   sleep 1
 done
