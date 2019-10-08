d9261528577029160c1b08668f33cee7444c0e81
Kaoru Fukumoto <k1fukumoto@gmail.com>
2019-10-08 08:50:34 +0000

    Clean-up for end-to-end automation

--- deploy/watch_capx_system.sh
+++ deploy/watch_capx_system.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+CLUSTER=$1
+
+export KUBECONFIG=$HOME/.kube/kind-config-$CLUSTER
+
 kubectl logs -n capv-system $(kubectl -n capv-system get po -o jsonpath='{.items..metadata.name}') -f
