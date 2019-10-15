49ab7e950049aaac9bcd7acd89c439816c5d0d6f
Kaoru Fukumoto <k1fukumoto@gmail.com>
2019-10-15 09:14:36 +0000

    fix path for envvar.sh

--- deploy/generate_manifests.sh
+++ deploy/generate_manifests.sh
@@ -9,18 +9,16 @@ export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
 
 CLUSTER=$1
 
-cd $HOME/tanzu-lab/deploy
-
 generate_manifest() {
   docker run --rm \
     -v "$(pwd)":/out \
-    -v "$(pwd)/envvars.sh":/envvars.txt:ro \
+    -v "$(pwd)/../envvars.sh":/envvars.txt:ro \
     gcr.io/cluster-api-provider-vsphere/release/manifests:v0.5.1 \
     -c $CLUSTER
 }
 
 patch_manifest() {
-  tmp=$(mktemp /tmp/$0.XXXX)
+  tmp=$(mktemp /tmp/patch_manifest.XXXX)
   for f in $(ls out/$CLUSTER/*.yaml)
   do
     sed $'s/sddc-cgw-horizon-cpsbu-desktops-1/\'sddc-cgw-horizon-cpsbu-desktops-1 \'/g' $f > $tmp
@@ -29,7 +27,5 @@ patch_manifest() {
   rm $tmp
 }
 
-# rm -frv out
 generate_manifest
 patch_manifest
-
