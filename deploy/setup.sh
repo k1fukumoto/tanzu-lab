cabc4979bfeb19a1cc20be360eb793d08190fc68
Kaoru Fukumoto <k1fukumoto@gmail.com>
2019-10-15 07:46:02 +0000

    another try to skip libssl installation prompt

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+sudo apt install -y debconf-utils
+echo 'libssl1.1	libraries/restart-without-asking	boolean	true' | sudo debconf-set-selections
+echo 'libssl1.1:amd64	libraries/restart-without-asking	boolean	true' | sudo debconf-selections
+
 sudo apt install -y scons
 sudo scons cli-tools
