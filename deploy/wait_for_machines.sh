b6be0dc09507eaad447e3e7d26d9bcf47383416d
Kaoru Fukumoto <k1fukumoto@gmail.com>
2019-10-17 10:14:16 +0000

    restructure scons dirs

---
+++ deploy/wait_for_machines.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+WORKLOAD_CLUSTER=$1
+
+echo -n "Start waiting for machines to be ready"
+while true
+do
+    RUNNING=1
+    for s in $(kubectl get machines --no-headers | awk '{print $NF}')
+    do
+	if [ "$s" != "running" ]; then
+	    RUNNING=0
+	fi 
+    done
+    if [ $RUNNING -eq 1 ]; then
+	break
+    fi
+    echo -n "."
+    sleep 3
+done
+
+kubectl get secret $WORKLOAD_CLUSTER-kubeconfig \
+  -o=jsonpath='{.data.value}' | \
+  { base64 -d 2>/dev/null || base64 -D; } \
+  > workload-cluster/out/$WORKLOAD_CLUSTER/kubeconfig
+
+export KUBECONFIG=workload-cluster/out/$WORKLOAD_CLUSTER/kubeconfig
+
+kubectl apply -f workload-cluster/out/$WORKLOAD_CLUSTER/addons.yaml
+
+kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.8.1/manifests/metallb.yaml
+kubectl apply -f metallb.yaml # this manifest needs to be generated from envvars.sh
+
+
