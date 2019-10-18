3dc2f2d73728d340cd7f98a7787b3bc6e04d2eb3
Kaoru Fukumoto <k1fukumoto@gmail.com>
2019-10-18 06:25:32 +0000

    Add functions to switch cluster

--- deploy/shortcuts.sh
+++ deploy/shortcuts.sh
@@ -1,9 +1,22 @@
-M_DIR=$HOME/tanzu-lab/deploy/out
-cc() {
-  C=$1
-  export KUBECONFIG=$M_DIR/$C/kubeconfig
+kubecfg_management() {
+  export DEPLOY=$HOME/tanzu-lab/deploy
+
+  export MANAGEMENT_CLUSTER=''
+  for f in $(ls -d $DEPLOY/management-cluster/out/*)
+  do
+    MANAGEMENT_CLUSTER=$(basename $f)
+    break
+  done
+
+  export KUBECONFIG=$DEPLOY/management-cluster/out/$MANAGEMENT_CLUSTER/kubeconfig
+  echo KUBECONFIG=$KUBECONFIG
 }
 
+kubecfg_workload() {
+  export KUBECONFIG=$DEPLOY/workload-cluster/out/$1/kubeconfig
+  echo KUBECONFIG=$KUBECONFIG
+}
+  
 source <(kubectl completion bash)
 
 alias k=kubectl
