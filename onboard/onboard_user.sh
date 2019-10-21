1178cd31511006a1922bbf2b99831101a62a80c8
Kaoru Fukumoto <k1fukumoto@gmail.com>
2019-10-21 08:49:28 +0000

    pass config path instead of cluster name

--- onboard/onboard_user.sh
+++ onboard/onboard_user.sh
@@ -8,17 +8,17 @@ echo "$USER:$PASS" | sudo chpasswd
 sudo gpasswd -a $USER docker
 sudo su $USER  -c 'ssh-keygen -t rsa -N "" -f /home/$USER/.ssh/id_rsa'
 
-find_management_cluster() {
-  for f in $(ls -d $HOME/tanzu-lab/deploy/management-cluster/out/*)
+find_management_kubecfg() {
+  for f in $(ls $HOME/tanzu-lab/deploy/management-cluster/out/*/kubeconfig)
   do
-    echo $(basename $f)
+    echo $f
     exit
   done
   echo 'WARNING: No management cluster found'
-  exit -1
+  exit 1
 }
 
 cd /home/$USER
 sudo su $USER -c 'git clone https://github.com/k1fukumoto/tanzu-lab.git'
-sudo su $USER -c "./tanzu-lab/onboard/onboard_user_2.sh $USER $(find_management_cluster)"
+sudo su $USER -c "./tanzu-lab/onboard/onboard_user_2.sh $USER $(find_management_kubecfg)"
 
--- onboard/onboard_user_2.sh
+++ onboard/onboard_user_2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 USER=$1
-M_CLUSTER=$2
+M_CFG=$2
 
 cd /home/$USER/tanzu-lab
 
@@ -9,6 +9,6 @@ tmp=$(mktemp /tmp/onboard_user.XXXX)
 cat ./deploy/envvars.sh | RSAKEY=$(cat /home/$USER/.ssh/id_rsa.pub) ./onboard/embed_key.py > $tmp
 mv $tmp ./deploy/envvars.sh
 
-cd ./deploy
-MANAGEMENT_CLUSTER=$M_CLUSTER WORKLOAD_CLUSTER=w-$USER scons workload-cluster
+cd ./deploy/workload-cluster
+KUBECONFIG=$M_CFG WORKLOAD_CLUSTER=w-$USER scons
 
