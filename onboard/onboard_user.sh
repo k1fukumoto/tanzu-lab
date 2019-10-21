650ac292f9b02fc2497a0648e02589fa254bd767
Kaoru Fukumoto <k1fukumoto@gmail.com>
2019-10-21 09:03:30 +0000

    make management cfg read-able

--- onboard/onboard_user.sh
+++ onboard/onboard_user.sh
@@ -18,7 +18,10 @@ find_management_kubecfg() {
   exit 1
 }
 
+CFG=$(find_management_kubecfg)
+sudo chmod a+r $CFG
+
 cd /home/$USER
 sudo su $USER -c 'git clone https://github.com/k1fukumoto/tanzu-lab.git'
-sudo su $USER -c "./tanzu-lab/onboard/onboard_user_2.sh $USER $(find_management_kubecfg)"
+sudo su $USER -c "./tanzu-lab/onboard/onboard_user_2.sh $USER $CFG"
 
