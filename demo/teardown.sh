895a5dd3f20fbd5ffe01a0aa6ef9b84167b91237
Kaoru Fukumoto <k1fukumoto@gmail.com>
2019-10-26 01:56:29 +0000

    add teardown script for demo

--- demo/teardown.sh
+++ demo/teardown.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
-kubectl delete all --selector run=kuard
+USER=$1
+
+DEPLOY=../deploy
+
+. $DEPLOY/shortcuts.sh
+
+sudo echo This script needs root privledge
+
+export KUBECONFIG=$DEPLOY/management-cluster/out/$(find_management_cluster)/kubeconfig
+for md in $(kubectl get machinedeployments -o=custom-columns=:.metadata.name | grep w-$USER)
+do
+  kubectl delete machinedeployments $md
+done
+
+for c in $(kubectl get cluster -o=custom-columns=:.metadata.name | grep w-$USER)
+do
+  kubectl delete cluster $c
+done
+
+cd ../onboard
+./teardown.sh $USER
+
+#export KUBECONFIG=$DEPLOY/workload-cluster/out/$1/kubeconfig
+
+
+# delete all pods
+#kubectl delete all --selector run=kuard
