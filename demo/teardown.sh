aa805e6e0f07da800f422d5d15638db32df526f5
Kaoru Fukumoto <k1fukumoto@gmail.com>
2019-12-30 04:20:57 +0000

    consolidate the teardown logic to onboard

--- demo/teardown.sh
+++ demo/teardown.sh
@@ -2,31 +2,10 @@
 
 USERS=$*
 
-DEPLOY=../deploy
-
-. $DEPLOY/shortcuts.sh
-sudo echo This script needs root privledge
+cd ../onboard
 
 for u in $USERS
 do
-
-    export KUBECONFIG=$DEPLOY/management-cluster/out/$(find_management_cluster)/kubeconfig
-    for md in $(kubectl get machinedeployments -o=custom-columns=:.metadata.name | grep w-$u)
-    do
-	kubectl delete machinedeployments $md
-    done
-
-    for c in $(kubectl get cluster -o=custom-columns=:.metadata.name | grep w-$u)
-    do
-	kubectl delete cluster $c
-    done
-
-    cd ../onboard
     ./teardown.sh $u
 done
 
-#export KUBECONFIG=$DEPLOY/workload-cluster/out/$1/kubeconfig
-
-
-# delete all pods
-#kubectl delete all --selector run=kuard
--- onboard/teardown.sh
+++ onboard/teardown.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+DEPLOY=../deploy
+
+. $DEPLOY/shortcuts.sh
+sudo echo This script needs root privledge
+
 for u in $*
 do 
-  sudo deluser $u
-  sudo rm -frv /home/$u
+    export KUBECONFIG=$DEPLOY/management-cluster/out/$(find_management_cluster)/kubeconfig
+    for md in $(kubectl get machinedeployments -o=custom-columns=:.metadata.name | grep w-$u)
+    do
+	kubectl delete machinedeployments $md
+    done
+
+    for c in $(kubectl get cluster -o=custom-columns=:.metadata.name | grep w-$u)
+    do
+	kubectl delete cluster $c
+    done
+
+    sudo deluser $u
+    sudo rm -frv /home/$u
 done
