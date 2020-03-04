cdc58bceaceeb208959985654fa0102cd8dc8881
Kaoru Fukumoto <k1fukumoto@gmail.com>
2020-03-04 14:09:03 +0900

    install kubectl

---
+++ deploy/management-cluster/install_kubectl.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+wget https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+
+chmod +x ./kubectl
+
+[ -d bin ] || (mkdir bin && chmod 777 bin)
+mv ./kubectl bin/
+
+bin/kubectl version --client
