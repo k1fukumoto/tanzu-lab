c66a2406739309296f505027fb34ed75378edc12
Kaoru Fukumoto <k1fukumoto@gmail.com>
2020-03-04 18:31:41 +0900

    add workload cluster creation script

---
+++ deploy/management-cluster/create_workload_cluster.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+export PATH=$PATH:bin
+
+clusterctl config cluster vsphere-quickstart \
+    --infrastructure vsphere:v0.6.0-rc.1 \
+    --kubernetes-version v1.17.3 \
+    --control-plane-machine-count 1 \
+    --worker-machine-count 1
+
