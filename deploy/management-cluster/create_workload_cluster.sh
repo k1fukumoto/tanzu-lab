609919435b6b97743830cbd7ea1725f7ffd4a3ee
Kaoru Fukumoto <k1fukumoto@gmail.com>
2020-03-05 07:27:56 +0900

    cleanup not to leave temp files

--- deploy/management-cluster/create_workload_cluster.sh
+++ deploy/management-cluster/create_workload_cluster.sh
@@ -8,7 +8,5 @@ clusterctl config cluster vsphere-quickstart \
     --infrastructure vsphere:v0.6.0-rc.1 \
     --kubernetes-version v1.17.3 \
     --control-plane-machine-count 1 \
-    --worker-machine-count 1 > cluster.yaml
-
-kubectl apply -f cluster.yaml
+    --worker-machine-count 1 | kubectl apply -f - 
 
--- deploy/management-cluster/install_clusterctl.sh
+++ deploy/management-cluster/install_clusterctl.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-wget https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
-
 wget https://github.com/kubernetes-sigs/cluster-api/releases/download/v0.3.0-rc.2/clusterctl-linux-amd64
 
 chmod +x ./clusterctl-linux-amd64
