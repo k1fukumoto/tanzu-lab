19257941b253a196ba45d878148f822b8ae4f28a
Kaoru Fukumoto <k1fukumoto@gmail.com>
2020-03-18 00:50:08 +0900

    update network spec

--- demo/kuard_query.sh
+++ demo/kuard_query.sh
@@ -2,7 +2,7 @@
 
 while true
 do
-  POD=$(curl -s http://10.108.5.201:8080 | grep pageCon | sed s/^.*\{/{/ |  python3 -c "import sys, json; print(json.load(sys.stdin)['hostname'])")
+  POD=$(curl -s http://10.108.10.201:8080 | grep pageCon | sed s/^.*\{/{/ |  python3 -c "import sys, json; print(json.load(sys.stdin)['hostname'])")
   IMAGE=$(kubectl get pod $POD -o jsonpath="{.spec.containers[0]['image']}")
 
   echo
