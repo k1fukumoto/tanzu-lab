f1dcd58cb474032d14c1204ad00f50300f745982
Kaoru Fukumoto <k1fukumoto@gmail.com>
2020-03-19 13:24:11 +0900

    make it more interactive

--- demo/tkg.sh
+++ demo/tkg.sh
@@ -4,17 +4,26 @@ set -e
 
 rm -frv ~/.kube ~/.tkg
 
-tkg init -v 5 --infrastructure vsphere:v0.6.0 --flavor dev
+# first run will always fail
+tkg init -v 5 --infrastructure vsphere:v0.6.0 --flavor dev || true
 cat ../deploy/cli-tools/tkg.yaml >> ~/.tkg/config.yaml
-vi ~/.tkg/config.yaml
+cat ~/.tkg/config.yaml
+read a
 
-time tkg init -v 5 --infrastructure vsphere:v0.6.0 --flavor dev
+# second run with fully populated config file
+tkg init -v 5 --infrastructure vsphere:v0.6.0 --flavor dev
 kubectl get nodes
+read a
 
-time tkg create cluster dev-01 -b --flavor=dev --kubernetes-version=1.16.2 --worker-machine-count=3
+# creat workload cluster
+tkg create cluster dev-01 -b --flavor=dev --kubernetes-version=1.16.2 --worker-machine-count=3
 tkg get cluster
 kubectl config get-contexts
 kubectl get nodes
+read a
 
 ./0_kuard_run.sh
 ./1_kuard_scale.sh
+
+time tkg create cluster dev-02 -b --flavor=dev --kubernetes-version=1.16.2 --worker-machine-count=3
+
